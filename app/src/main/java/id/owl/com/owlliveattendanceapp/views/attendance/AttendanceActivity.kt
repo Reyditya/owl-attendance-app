32091c824ac3bb852da51ce59c13fb38af88a57a
Reyditya <dirhaam06@gmail.com>
2022-11-12 01:28:42 +0700

    ui: prepare AttendanceActivity for display 3 fragment

---
+++ app/src/main/java/id/owl/com/owlliveattendanceapp/views/attendance/AttendanceActivity.kt
@@ -0,0 +1,55 @@
+package id.owl.com.owlliveattendanceapp.views.attendance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import id.owl.com.owlliveattendanceapp.R
+import id.owl.com.owlliveattendanceapp.databinding.ActivityAttendanceBinding
+import id.owl.com.owlliveattendanceapp.views.fragment.attendance.AttendanceFragment
+import id.owl.com.owlliveattendanceapp.views.fragment.history.HistoryFragment
+import id.owl.com.owlliveattendanceapp.views.fragment.profile.ProfileFragment
+
+class AttendanceActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAttendanceBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAttendanceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    private fun init() {
+        binding.bnAttendance.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_history -> {
+                    openFragment(HistoryFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.action_attendance -> {
+                    openFragment(AttendanceFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+                R.id.action_profile -> {
+                    openFragment(ProfileFragment())
+                    return@setOnNavigationItemSelectedListener true
+                }
+            }
+            return@setOnNavigationItemSelectedListener false
+        }
+        openDefaultFragment()
+    }
+
+    private fun openDefaultFragment() {
+        binding.bnAttendance.selectedItemId = R.id.action_attendance
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fl_attendance, fragment)
+            .addToBackStack(null)
+            .commit()
+    }
+}
